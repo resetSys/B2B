4dfa731fded86209bbea00f1185ca110ba010808
User大帅 <git config --global user.email you@example.com>
2020-08-06 08:49:01 +0800

    使用if替代了switch

--- src/mixins/filters/formatStatus.js
+++ src/mixins/filters/formatStatus.js
@@ -2,28 +2,22 @@ var formatStatus = {
   filters:{
     /**转换tips状态 */
     formatType(num){
-      switch (num) {
-        case 1:
-          return "danger"    
-        case 2:
-          return "success"
-        case 0:
-          return "info"
-        default:
-          break;
+      if (num == 1) {
+        return "danger"  
+      } else if(num == 2) {
+        return "success"
+      } else {
+        return "info"
       }
     },
     /**转换状态码 */
     formatStatus(num){
-      switch (num) {
-        case 1:
-          return "禁用中"
-        case 2:
-          return "启用中"
-        case 0:
-          return "已删除"
-        default:
-          break;
+      if (num == 1) {
+        return "禁用中"  
+      } else if(num == 2) {
+        return "启用中"
+      } else {
+        return "已删除"
       }
     }
   }
