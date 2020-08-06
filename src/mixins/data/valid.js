6be37174f533763e369d906d838331d219e2bdf2
User大帅 <git config --global user.email you@example.com>
2020-08-06 18:21:03 +0800

    添加数字匹配表单验证

--- src/mixins/data/valid.js
+++ src/mixins/data/valid.js
@@ -2,6 +2,18 @@
  * 表单验证公共字段
  */
 var mustFill = { required: true, message: '该字段为必填项', trigger: 'blur' };
+var mustNumber = {
+  validator:(rule, value, callback) => {
+    var reg = /^[0-9]*$/;
+    if (!reg.test(value)) {
+      return callback(new Error('输入内容只能为整数'));
+    } else {
+      callback();
+    }
+  },
+  trigger: 'blur' 
+}
 export {
-  mustFill
+  mustFill,
+  mustNumber
 }
