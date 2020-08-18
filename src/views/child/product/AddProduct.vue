e5b3b50821746f84569de490521b60797d2ebb92
User大帅 <git config --global user.email you@example.com>
2020-08-18 11:36:46 +0800

    对接图片上传、删除、商品提交

--- src/views/child/product/AddProduct.vue
+++ src/views/child/product/AddProduct.vue
@@ -14,14 +14,22 @@
               :options="classifyList"
               :props="{label:'label',value:'value',children:'children'}"
               style="width:200px"
+              :checkStrictly="true"
+              :emitPath="false"
               :show-all-levels="false">
             </el-cascader>
           </el-form-item>
         </div>
         <div class="form-item-wrap">
           <el-form-item label="商品楼层" prop="floor">
-            <el-input class="form-input" v-model="goodForm.floor" clearable
-              style="width:200px;"></el-input>
+            <el-select v-model="goodForm.floor">
+              <el-option
+                v-for="item in floorList"
+                :key="item.value"
+                :value="item.value"
+                :label="item.label">
+              </el-option>
+            </el-select>
           </el-form-item>
         </div>
         <div class="form-item-wrap">
@@ -57,10 +65,11 @@
               <el-image
                 class="imgView-img"
                 :src="coverImg"
-                fit="cover"></el-image>
+                fit="cover"
+                :preview-src-list="[coverImg]"></el-image>
             </div>
             <upload @fileChange="uploadCallBack" 
-              accept="image/jpg,image/jpeg">
+              accept="image/jpg,image/jpeg,image/png">
             </upload>
           </el-form-item>
         </div>
@@ -71,11 +80,11 @@
               :key="index">
               <el-image
                 class="imgView-img"
-                :src="item"
+                :src="item.url"
                 fit="cover"
-                :preview-src-list="slideList"></el-image>
+                :preview-src-list="viewSlideList"></el-image>
               <span class="imgView-del el-icon-delete"
-                @click="handleDelSlide(item)"></span>
+                @click="handleDelSlide(item.id)"></span>
             </div>
             <upload @fileChange="uploadCallBack2"
               accept="image/jpg,image/jpeg,image/png"
@@ -104,6 +113,9 @@ import WangEditor from "@/components/WangEditor.vue";
 import Upload from "@/components/Upload.vue";
 //网络
 import { request } from "@/request";
+import { uploadRequest } from "@/request";
+//表单验证
+import { mustFill } from "@/mixins/data/valid.js";
 
 export default {
   name: 'addProduct',
@@ -116,6 +128,8 @@ export default {
       /**商品表单 */
       //商品分类 商品楼层 价格 品牌 描述 排序 封面图 商品详情图
       goodForm:{
+        organId:"",
+        articleId:"",
         classify:"",
         floor:"",
         price:"",
@@ -126,19 +140,21 @@ export default {
         slides:""
       },
       goodFormRule:{
-        classify:[],
-        floor:[],
-        price:[],
-        brand:[],
-        sort:[],
-        des:[]
+        classify:[mustFill],
+        floor:[mustFill],
+        price:[mustFill],
+        // brand:[mustFill],
+        sort:[mustFill],
+        des:[mustFill]
       },
       /**存放封面图img临时地址 */
       coverImg:"",
       /**存放商品详情图 */
       slideList:[],
       /**分类列表 */
-      classifyList:[]
+      classifyList:[],
+      /**楼层列表选项 */
+      floorList:[]
     }
   },
   components: {
@@ -147,13 +163,21 @@ export default {
   },
   mounted(){
     //接收role传递参数
-    let goodsId = JSON.parse(decodeURIComponent(this.$route.query.goodsId));
-    if (goodsId) {
+    let prams = JSON.parse(decodeURIComponent(this.$route.query.prams));
+    if (prams) {
       //执行编辑，没有注册
-      this.goodsId = "114524";
+      this.goodsId = prams.goodsId;
+      this.goodForm.organId = prams.organId;
       this.getGoodData();
-      this.getDetailImg();
       this.getClassify();
+    }
+  },
+  computed:{
+    /**返回详情图片地址 */
+    viewSlideList(){
+      return this.slideList.map((item)=>{
+        return item.url
+      })
     }
   },
   methods:{
@@ -173,7 +197,30 @@ export default {
           articleId:this.goodsId
         },
       }).then((res) => {
-        console.log(res);
+        let {Message,Data,MsgCode,Success} = res.data.models;
+        if (Success) {
+          this.goodForm.articleId = Data[0].ArticleId;
+          this.goodForm.classify = Data[0].CategoryId;
+          this.goodForm.floor = Data[0].Attribute[0].FloorId;
+          this.goodForm.price = Data[0].Price;
+          this.goodForm.brand = Data[0].BrandCode;
+          this.goodForm.des = Data[0].Content;
+          this.goodForm.sort = Data[0].Sort;
+          this.coverImg = Data[0].ImgUrl;
+          Data[0].ImgList.forEach(ele => {
+            this.slideList.push({
+              url:ele.ImgUrl,
+              id:ele.ImgId
+            });
+          });
+        } else {
+          this.$message({
+            message: '获取详情失败:'+MsgCode+Message,
+            type: 'error'
+          });
+        }
+      }).catch((err)=>{
+        console.log(err);
       })
     },
     /**获取商品详情图 */
@@ -187,7 +234,21 @@ export default {
           articleId:this.goodsId
         },
       }).then((res) => {
-        console.log(res);
+        let {Message,Data,MsgCode,Success} = res.data.models;
+        this.slideList = [];
+        if (Success) {
+          Data.forEach(ele => {
+            this.slideList.push({
+              url:ele.ImgUrl,
+              id:ele.ImgId
+            });
+          });
+        } else {
+          this.$message({
+            message: '获取商品详情图片失败:'+MsgCode+Message,
+            type: 'error'
+          });
+        }
       })
     },
     /**获取商品分类数据 */
@@ -202,7 +263,9 @@ export default {
       }).then((res) => {
         let {Success,Data} = res.data.models;
         let categoryList = Data[0].CategoryList;
+        let attributeList= Data[0].AttributeList;
         this.classifyList = [];
+        this.floorList = [];
         if (Success) {
           categoryList.forEach((ele,index) => {
             this.classifyList.push({
@@ -219,18 +282,18 @@ export default {
               });
             }
           });
+          attributeList.forEach(ele => {
+            this.floorList.push({
+              value:ele.AttributeId,
+              label:ele.Title
+            })
+          });
         }
       }).catch((err) => {
         window.console.log(err);
       });
     },
 
-    handleDelSlide(item){
-      this.$message({
-        message: '删除'+item,
-        type: 'error'
-      });
-    },
     /**提交表单 */
     submitForm(){
       this.$refs['addForm'].validate((valid)=>{
@@ -241,14 +304,14 @@ export default {
             data:{
               entId:this.organId,
               userId:this.adminId,
-              xEntId:"",
-              articleId:this.goodForm.classify,
-              categoryId:"",
-              attribute:"",
-              price:"",
-              brandCode:"",
-              content:"",
-              sort:""
+              xEntId:this.goodForm.organId,
+              articleId:this.goodForm.articleId,
+              categoryId:this.goodForm.classify[1],
+              attribute:this.goodForm.floor,
+              price:this.goodForm.price,
+              brandCode:this.goodForm.brand,
+              content:this.goodForm.des,
+              sort:this.goodForm.sort
             },
           }).then((res) => {
             let {Success,MsgCode,Message} = res.data.models;
@@ -258,7 +321,6 @@ export default {
                 message: '操作成功',
                 type: 'success'
               });
-
             } else {
               this.$message({
                 message: '操作失败'+MsgCode+Message,
@@ -280,36 +342,99 @@ export default {
     resetForm(){
       this.$refs['addForm'].resetFields();
     },
+    /**删除详情图片 */
+    handleDelSlide(id){
+      this.$confirm('确定删除吗?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'error'
+      }).then(() => {
+        request({
+          url:"HTGoodsAdmin/DltGoodsImg",
+          method:"post",
+          data:{
+            entId:this.organId,
+            userId:this.adminId,
+            imgId:id
+          },
+        }).then((res) => {
+          let {Success,MsgCode,Message} = res.data.models;
+            if (Success) {
+              this.$message({
+                message: '操作成功',
+                type: 'success'
+              });
+              this.getDetailImg();
+            } else {
+              this.$message({
+                message: '操作失败'+MsgCode+Message,
+                type: 'error'
+              });
+            }
+        }).catch((err) => {
+          console.log(err);
+        });
+      });
+    },
+
 
     /**文件上传回调 */
     uploadCallBack(file){
       if (file.length) {
-        this.goodForm.cover = file[0];
+        //将上传的图片file对象直接转化为blob地址，无需重新获取商品详情
         this.coverImg = URL.createObjectURL(file[0])
-        request({
-            url:"HTGoodsAdmin/SaveGoodsInfo",
-            method:"post",
-            data:{
-              entId:this.organId,
-              userId:this.adminId,
-              articleId:this.goodsId,
-              imgType:"",
-              file:this.coverImg
-            }
-        }).then((res) => {
-          console.log(res);
-        }).catch((err) => {
-          console.log(err)
-        });
+        this.uploadImage('Goods',file);
       }
     },
     uploadCallBack2(file){
-      this.slideList = [];
-      this.goodForm.slides = file;
-      file.forEach(ele => {
-        this.slideList.push(URL.createObjectURL(ele));
+      this.uploadImage('GoodsDetail',file);
+      //商品详情图片是叠加的，需要重新请求数据
+      this.getDetailImg();
+    },
+    /**商品图片上传 */
+    uploadImage(type,file){
+      let data = {
+        entId:this.organId,
+        userId:this.adminId,
+        articleId:this.goodsId,
+        imgType:type
+      }
+      let formData = new FormData();
+      for (const key in data) {
+        if (Object.prototype.hasOwnProperty.call(data,key)) {
+          formData.append(key,data[key]);
+        }
+      }
+      //将file数组添加到formData实例中
+      file.forEach((ele,index) => {
+        formData.append('file'+index,ele);
       });
-      
+
+      uploadRequest({
+        url:"HTGoodsAdmin/GoodsImgUpload",
+        method:"post",
+        data:formData
+      }).then((res) => {
+        //图片上传成功提示
+        let {Message,MsgCode,Success} = res.data.models;
+        if (Success) {
+          this.$message({
+            message: '图片上传成功',
+            type: 'success'
+          });
+        } else {
+          this.$message({
+            message: '图片上传失败:'+MsgCode+Message,
+            type: 'error'
+          });
+        }
+        //如果是详情图片上传刷新
+        if (type == "GoodsDetail") {
+          this.getDetailImg();
+        }
+      }).catch((err) => {
+        console.log(err);
+      });
     },
     /**富文本编辑内容改变回调 */
     change(val) {
