85db7569d0990e0607fa61a1adf2d1d57bf37906
User大帅 <git config --global user.email you@example.com>
2020-08-18 11:36:04 +0800

    新增文件传输请求axios实例

--- src/request/index.js
+++ src/request/index.js
@@ -12,7 +12,7 @@ import Qs from "qs";
 //导出第一个axios实例
 export function request (config){
   let instence = axios.create({
-    timeout:5000,
+    timeout:10000,
     baseURL:"http://47.105.206.10:8094/",
     headers: {
       'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'
@@ -23,6 +23,39 @@ export function request (config){
     }],
   });
 
+  instence.interceptors.request.use(function (res) {
+    //请求开始
+    //添加loading
+    vm.$store.commit('handleLoding',{status:true});
+    return res;
+  },function (err) {
+    //
+    vm.$store.commit('handleLoding',{status:false});
+    return err;
+  });
+  instence.interceptors.response.use(function (res) {
+    //响应成功
+    //关闭loading
+    vm.$store.commit('handleLoding',{status:false});
+    return res;
+  },function (err) {
+    //
+    vm.$store.commit('handleLoding',{status:false});
+    return err;
+  });
+
+  return instence(config)
+}
+
+//上传文件时使用的请求实例
+export function uploadRequest(config) {
+  let instence = axios.create({
+    timeout:10000,
+    baseURL:"http://47.105.206.10:8094/",
+    headers: {
+      'Content-Type':'multipart/form-data'
+    },
+  });
   instence.interceptors.request.use(function (res) {
     //请求开始
     //添加loading
